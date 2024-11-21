9ca6502fa8316b11796edcb9facb4c2dd116a733
Aayush <136212833+Aayush-Roy@users.noreply.github.com>
2024-11-21 11:59:59 +0530

    minor changes

--- my-youtube/src/utils/Constraints.jsx
+++ my-youtube/src/utils/Constraints.jsx
@@ -1,6 +1,6 @@
-export const API_KEY = "AIzaSyBz1IaysMIW4MHPGAx1cgh71MhlzutHV08";
+export const API_KEY = "PUT_YOUR_API_KEY";
 export const  API_URL = `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=IN&maxResults=5&key=${API_KEY}`;
 // /xport const YOUTUBE_SEARCH_API = "https://cors-anywhere.herokuapp.com/https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-export const YOUTUBE_SEARCH_API = "https://youtube.googleapis.com/youtube/v3/search";
+export const YOUTUBE_SEARCH_API = "PUT_YOUR_API_KEY";
 // const API_KEY = "YOUR_API_KEY"; // Replace with your actual API key
